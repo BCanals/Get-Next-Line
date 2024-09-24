ccebc27e0a37ae0308a5eb487913a4f0f11df2aa
Bernat Canals I Guàrdia <bcanals-@car8s5.42barcelona.com>
2024-09-24 20:53:00 +0200

    S3E1

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: bcanals- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 17:14:16 by bcanals-          #+#    #+#             */
-/*   Updated: 2024/09/17 13:23:28 by bcanals-         ###   ########.fr       */
+/*   Updated: 2024/09/24 18:31:22 by bcanals-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#ifndef GET_NEXT_LINE
-# define GET_NEXT_LINE
+# include <stddef.h>
 
 char	*ft_read(int fd, char *buffer);
 char	*get_next_line(int fd);
@@ -21,5 +21,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_calloc(size_t size);
 size_t	ft_strlen(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(char *s, int c);
 
-# endif
+#endif
