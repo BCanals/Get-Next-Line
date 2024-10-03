938b8fe5b6d0288c6fee1dcd6a22d41e1c15ea50
BCanals <bizcru@DESKTOP-SAU74E1>
2024-10-03 19:02:37 +0200

    S3E2

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: bcanals- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 16:22:49 by bcanals-          #+#    #+#             */
-/*   Updated: 2024/09/24 20:47:34 by bcanals-         ###   ########.fr       */
+/*   Updated: 2024/10/03 19:00:01 by bizcru           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,11 @@ char	*ft_next(char *buffer, char *line)
 	buf_len = ft_strlen(buffer) - line_len;
 	new_buffer = ft_calloc(buf_len + 1);
 	if (!new_buffer)
+	{
+		free(buffer);
+		free(line);
 		return (NULL);
+	}
 	new_buffer = ft_memcpy(new_buffer, buffer + line_len, buf_len);
 	free(buffer);
 	return (new_buffer);
@@ -62,16 +66,16 @@ char	*ft_read(int fd, char *buffer)
 		read_bytes = read(fd, reading, BUFFER_SIZE);
 		if (read_bytes < 0)
 		{
+			free(buffer);
 			free(reading);
 			return (NULL);
 		}
 		else if (read_bytes == 0)
 		{
 			free(reading);
-			break ;
+			return (buffer);
 		}
-		else
-			buffer = ft_strjoin(buffer, reading);
+		buffer = ft_strjoin(buffer, reading);
 	}
 	return (buffer);
 }
@@ -90,9 +94,15 @@ char	*get_next_line(int fd)
 	buffer = ft_read(fd, buffer);
 	if (!buffer)
 		return (NULL);
+	if (!*buffer)
+	{
+		return (NULL);
+	}
 	line = ft_line(buffer);
 	if (!line)
 		return (NULL);
 	buffer = ft_next(buffer, line);
+	if (!buffer)
+		return (NULL);
 	return (line);
 }
